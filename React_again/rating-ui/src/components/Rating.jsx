41c11835654467319ba56d172fb61f624fce0367
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-05 17:41:30 +0300

    Auto push changes 2026-07-05 17:41:30

--- React_again/rating-ui/src/components/Rating.jsx
+++ React_again/rating-ui/src/components/Rating.jsx
@@ -1,12 +1,24 @@
 const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
+  const clicked = (index) => {
+    console.log("clicked", index + 1);
+  };
+
+  const hovered = (direction) => console;
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star) => (
-          <span key={star} className="star">
+        {stars.map((star, index) => (
+          <span
+            onMouseEnter={() => hovered("enter")}
+            onMouseLeave={() => hovered("leave")}
+            onClick={() => clicked(index)}
+            key={star}
+            className="star"
+          >
             {"\u2605"}
           </span>
         ))}
