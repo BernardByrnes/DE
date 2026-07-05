b8618e8c90323f9bf8690b40b8b2de88efdf1234
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-05 18:26:39 +0300

    Auto push changes 2026-07-05 18:26:39

--- React_again/rating-ui/src/components/Rating.jsx
+++ React_again/rating-ui/src/components/Rating.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Rating = () => {
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
@@ -10,7 +11,13 @@ const Rating = () => {
       <h2>Rate Your Experience</h2>
       <div className="stars">
         {stars.map((star) => (
-          <span onClick={() => setRating(star)} key={star} className="star">
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className={`star ${star <= (hover || rating) ? "active" : ""}`}
+          >
             {"\u2605"}
           </span>
         ))}
