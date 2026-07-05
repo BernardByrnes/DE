fba376748abec9417686ef024456a88989053150
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-05 17:26:26 +0300

    Auto push changes 2026-07-05 17:26:26

--- React_again/rating-ui/src/App.jsx
+++ React_again/rating-ui/src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import Rating from "./components/Rating";
 
 const App = () => {
-  const name = "Ben";
   return (
     <div>
-      <h1>Hello {name}</h1>
       <Rating />
     </div>
   );
