b9edd39b37c0a1646e3cc228620cc2485f7de4e6
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-06 17:48:19 +0300

    Auto push changes 2026-07-06 17:48:19

--- py_first/wee1-day2-tip-calculator/app.py
+++ py_first/wee1-day2-tip-calculator/app.py
@@ -1,3 +1,12 @@
+def calculate_tip(bill_amount, tip_percentage):
+    tip_rate = tip_percentage / 100
+    return bill_amount * tip_rate
+
+
+def calculate_total(bill_amount, tip_amount):
+    return bill_amount + tip_amount
+
+
 def main():
     bill_text = input("Enter Bill Amount: ")
     tip_text = input("Enter Tip Percentage: ")
@@ -5,8 +14,16 @@ def main():
     bill_amount = float(bill_text)
     tip_percentage = float(tip_text)
 
-    print(bill_amount)
-    print(tip_percentage)
+    tip_amount = calculate_tip(bill_amount, tip_percentage)
+    total_amount = calculate_total(bill_amount, tip_amount)
+
+    print("")
+    print("Tip Calculator Result")
+    print("----------------------")
+    print(f"Bill: ${bill_amount:.2f}")
+    print(f"Tip: ${tip_amount:.2f}")
+    print(f"Total: ${total_amount:.2f}")
+
 
 if __name__ == "__main__":
     main()
