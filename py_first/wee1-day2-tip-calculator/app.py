d94cf594fe2509c7d54efcae46c0845485df92fd
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-06 17:33:15 +0300

    Auto push changes 2026-07-06 17:33:15

--- py_first/wee1-day2-tip-calculator/app.py
+++ py_first/wee1-day2-tip-calculator/app.py
@@ -1,5 +1,12 @@
 def main():
-    print("Tip Calculator")
+    bill_text = input("Enter Bill Amount: ")
+    tip_text = input("Enter Tip Percentage: ")
+
+    bill_amount = float(bill_text)
+    tip_percentage = float(tip_text)
+
+    print(bill_amount)
+    print(tip_percentage)
 
 if __name__ == "__main__":
     main()
