72fbf1d07818b1caa8860565d1d2fb92655c64f0
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-06 18:03:25 +0300

    Auto push changes 2026-07-06 18:03:25

---
+++ py_first/wee1-day2-tip-calculator/day2_hw.py
@@ -0,0 +1,11 @@
+def calculate_tip(bill_amount, tip_amount)
+
+
+def main():
+    bill_text = input("What is your bill?")
+    tip_text = input("What Is The Tip Rate?")
+    
+    bill_amount = float(bill_text)
+    tip_amount = float(tip_text)
+    
+    
