a7137523aa09d1e5069309134bf65139dfe283a9
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-07 23:19:39 +0300

    Auto push changes 2026-07-07 23:19:38

--- py_first/week1-day3/app.py
+++ py_first/week1-day3/app.py
@@ -1,8 +1,21 @@
+def get_hint(guess, secret_number):
+    if guess < secret_number:
+        return "Too low Bro"
+    elif guess > secret_number:
+        return "Too high Bro"
+    else:
+        return "Correct"
+
+
 def main():
     secret_number = 7
+
     guess_text = input("Guess a number between 1 and 10: ")
     guess = int(guess_text)
-    print(f"You guessed {guess}")
+
+    hint = get_hint(guess, secret_number)
+    print(hint)
+
 
 if __name__ == "__main__":
     main()
