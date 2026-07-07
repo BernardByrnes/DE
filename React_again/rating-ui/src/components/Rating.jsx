1bfc338d344f9e501cf3b3bfac7a5b2a824b7974
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-07 13:34:10 +0300

    Auto push changes 2026-07-07 13:34:10

--- React_again/rating-ui/src/components/Rating.jsx
+++ React_again/rating-ui/src/components/Rating.jsx
@@ -51,17 +51,7 @@ const Rating = ({
       </button>
       {/* Modal */}
       {submitted && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <h2>Thank You</h2>
-            <p>
-              You rated us {rating} star{rating > 1 ? "s" : ""}.
-            </p>
-            <button className="close-btn" onClick={closeModal}>
-              Close
-            </button>
-          </div>
-        </div>
+        
       )}
     </div>
   );
---
+++ React_again/rating-ui/src/components/modal.jsx
@@ -0,0 +1,18 @@
+const modal = ({ isOpen, onClose, rating }) => {
+  if (!isOpen) return null;
+
+  return;
+  <div className="modal-overlay">
+    <div className="modal">
+      <h2>Thank You</h2>
+      <p>
+        You rated us {rating} star{rating > 1 ? "s" : ""}.
+      </p>
+      <button className="close-btn" onClick={closeModal}>
+        Close
+      </button>
+    </div>
+  </div>;
+};
+
+export default modal;
