509186e8c54cece730a47a40e0b351c278c27daa
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-07 23:34:46 +0300

    Auto push changes 2026-07-07 23:34:46

--- py_first/week1-day3/app.py
+++ py_first/week1-day3/app.py
@@ -9,12 +9,19 @@ def get_hint(guess, secret_number):
 
 def main():
     secret_number = 7
+    guess = 0
+    attempts = 0
 
-    guess_text = input("Guess a number between 1 and 10: ")
-    guess = int(guess_text)
+    print("Number Guessing Game")
+    print("-------------------------")
+    print("Guess a number between 1 and 10")
 
-    hint = get_hint(guess, secret_number)
-    print(hint)
+    while guess != secret_number:
+        guess_text = input("Enter Your Guess: ")
+        guess = int(guess_text)
+        attempts = attempts + 1
+        hint = get_hint(guess, secret_number)
+        print(hint)
 
 
 if __name__ == "__main__":
