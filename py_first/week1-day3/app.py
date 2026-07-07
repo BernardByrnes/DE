ccd87cdee737ee993b8fa3f206c05e3e6e85d4b5
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-07 21:49:31 +0300

    Auto push changes 2026-07-07 21:49:31

--- py_first/week1-day3/app.py
+++ py_first/week1-day3/app.py
@@ -1,1 +1,8 @@
-secret = 6
+def main():
+    secret_number = 7
+    guess_text = input("Guess a number between 1 and 10: ")
+    guess = int(guess_text)
+    print(f"You guessed {guess}")
+
+if __name__ == "__main__":
+    main()
