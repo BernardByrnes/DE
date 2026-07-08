4d52144e47571e5b1ad9531ac0a38e4c67b8a8a5
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-08 16:54:02 +0300

    Auto push changes 2026-07-08 16:54:02

--- py_first/week1-day4/app.py
+++ py_first/week1-day4/app.py
@@ -1,8 +1,16 @@
-guesses = [5, 8, 7]
+transaction_amounts = [19.99, 45.00, 100.50]
 
-for guess in guesses:
-    print(guess)
+records_processed = 0
+total_amount = 0
 
+for amount in transaction_amounts:
+    records_processed = records_processed + 1
+    total_amount = total_amount + amount
 
-summary = {"player_name": "Ben", "attempts": 3, "result": "won"}
-print(summary["player_name"])
+summary = {
+    "records_processed": records_processed,
+    "total_amount": total_amount,
+}
+
+print(f"Records Processed:  {summary['records_processed']}")
+print(f"Total Amount:  {summary['total_amount']}")
