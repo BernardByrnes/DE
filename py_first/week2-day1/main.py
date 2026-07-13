629eb4d98eb557c4039495e8a05aab0cc3ae836f
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-13 17:02:03 +0300

    Auto push changes 2026-07-13 17:02:03

--- py_first/week2-day1/main.py
+++ py_first/week2-day1/main.py
@@ -1,13 +1,34 @@
 import csv
 
+
+def read_students(file_path):
+    students = []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            students.append(row)
+    return students
+
+
+def print_students(students):
+    print("")
+    print("Students")
+    print("--------")
+
+    for student in students:
+        print(
+            f"{student['name']} is learning {student['course']}"
+            f"and has grade {student['grade']}"
+        )
+
+
 def main():
-    file_path = "student.csv"
-
+    file_path = "students.csv"
     students = read_students(file_path)
-
     print_students(students)
 
 
-
-if __name__ = "__main__":
-  main()
+if __name__ == "__main__":
+    main()
