a94a7f53ed980c9d21bd4d02dbca89e9efeb78ec
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-24 21:37:44 +0300

    Auto push changes 2026-07-24 21:37:44

---
+++ py_first/week2-day3/main.py
@@ -0,0 +1,11 @@
+def get_student():
+    name = input("Student name")
+    course = input("Course")
+    grade = input("Grade")
+
+    return {"name": name, "course": course, "grade": grade}
+
+
+def main():
+    student = get_student()
+    print(student)
