8cbe71e8ae36217448a9a961d41e5129e53570a5
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-07-24 22:52:54 +0300

    Auto push changes 2026-07-24 22:52:54

--- py_first/week2-day3/main.py
+++ py_first/week2-day3/main.py
@@ -19,10 +19,33 @@ def append_student(student):
         writer.writerow(student)
 
 
+def read_students(file_path="students.csv"):
+    students = []
+    with open(file_path, "r", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            students.append(row)
+    return students
+
+
+def print_students(students):
+    print("")
+    print("Students List")
+    print("-------------------")
+
+    for student in students:
+        print(
+            f"my name is{student['name']}, i am taking {student['name']}, i got {student['grade']}"
+        )
+
+
 def main():
     student = get_student()
     append_student(student)
     print("student saved")
+    students = read_students("studentz.csv")
+    print_students(students)
 
 
 if __name__ == "__main__":
