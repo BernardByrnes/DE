f2148ebf94e21e29d14b6686d296469b8c6e3f44
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-08-03 13:48:29 +0300

    Auto push changes 2026-08-03 13:48:29

--- py_first/week2-day6n7/app.py
+++ py_first/week2-day6n7/app.py
@@ -12,14 +12,19 @@ def read_file(file_path="teachers.csv"):
 
 
 def print_list(teachers):
+    number_of_records = 0
     print(" ")
     print("teachers")
     print("----------------------")
 
     for teacher in teachers:
+        number_of_records = number_of_records + 1
         print(
             f"my name is {teacher['name']}, i teach {teacher['subject']}, i have {teacher['students']} students in my class"
         )
+    print(
+        f"there is a total of {number_of_records} teachers"
+    )
 
 
 def main():
