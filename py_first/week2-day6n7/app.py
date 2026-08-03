fb2fa7dd78e3beb3ce7810ce0117cc13a6daf8c1
Bernard Byrnes <bernardtamboedu@gmail.com>
2026-08-03 13:33:28 +0300

    Auto push changes 2026-08-03 13:33:28

--- py_first/week2-day6n7/app.py
+++ py_first/week2-day6n7/app.py
@@ -11,9 +11,20 @@ def read_file(file_path="teachers.csv"):
     return teachers
 
 
+def print_list(teachers):
+    print(" ")
+    print("teachers")
+    print("----------------------")
+
+    for teacher in teachers:
+        print(
+            f"my name is {teacher['name']}, i teach {teacher['subject']}, i have {teacher['students']} students in my class"
+        )
+
+
 def main():
     teachers = read_file("teachers.csv")
-    print(teachers)
+    print_list(teachers)
 
 
 if __name__ == "__main__":
